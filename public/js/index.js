bbc39bb338f93b4245bd123998c9a82e3347a9ae
DavidAugustoMendes <davidslow522@gmail.com>
2025-11-23 12:57:41 -0300

    Adicionado Formatação Prettier

--- public/js/index.js
+++ public/js/index.js
@@ -10,88 +10,84 @@ checkLogged();
 
 //LOGAR CONTA
 
-document.getElementById("login-form").addEventListener("submit", function(e) {
-    e.preventDefault();
+document.getElementById("login-form").addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const email = document.getElementById("email-input").value;
-    const password = document.getElementById("password-input").value;
-    const checkSession = document.getElementById("session-check").checked;
-    const account = getAccount(email);
-        if(!account) {
-            alert("Opss! Verifique o usuário ou a senha.");
-            return;
-        }
+  const email = document.getElementById("email-input").value;
+  const password = document.getElementById("password-input").value;
+  const checkSession = document.getElementById("session-check").checked;
+  const account = getAccount(email);
+  if (!account) {
+    alert("Opss! Verifique o usuário ou a senha.");
+    return;
+  }
 
-        if(account){
-            if(account.password !== password){
-                alert("Opss! Verifique o usuário ou a senha.");
-                return;
-            }
+  if (account) {
+    if (account.password !== password) {
+      alert("Opss! Verifique o usuário ou a senha.");
+      return;
+    }
 
-            saveSession(email, checkSession);
-            window.location.href = "home.html";
-        }
+    saveSession(email, checkSession);
+    window.location.href = "home.html";
+  }
 });
 
+// CRIAR CONTA
+document.getElementById("create-form").addEventListener("submit", function (e) {
+  e.preventDefault();
 
- // CRIAR CONTA
- document.getElementById("create-form").addEventListener("submit", function(e) {
-    e.preventDefault();
-    
-    const email = document.getElementById("email-create-input").value;
-    const password = document.getElementById("password-create-input").value;    
+  const email = document.getElementById("email-create-input").value;
+  const password = document.getElementById("password-create-input").value;
 
-    if(email.length < 5){
-        alert("Preencha o campo de email válido.");
-        return;
-    }
+  if (email.length < 5) {
+    alert("Preencha o campo de email válido.");
+    return;
+  }
 
-    if(password.length < 4) {
-        alert("Preencha a senha com no mínimo 4 caracteres.");
-        return;
-    }
+  if (password.length < 4) {
+    alert("Preencha a senha com no mínimo 4 caracteres.");
+    return;
+  }
 
-    saveAccount({
-        login: email,
-        password: password,
-        transactions: []
-    })
+  saveAccount({
+    login: email,
+    password: password,
+    transactions: [],
+  });
 
-    myModal.hide();
-    alert("Conta criada com sucesso!");
-    
+  myModal.hide();
+  alert("Conta criada com sucesso!");
+});
 
- });
-
- function checkLogged() {
-    if(session) {
-        sessionStorage.setItem("logged", session);
-        logged = session;
-    }   
-    if(logged) {
-        saveSession(logged, session);
-        window.location.href = "home.html";
-    }
+function checkLogged() {
+  if (session) {
+    sessionStorage.setItem("logged", session);
+    logged = session;
+  }
+  if (logged) {
+    saveSession(logged, session);
+    window.location.href = "home.html";
+  }
 }
 
-
- function saveAccount(data) {
-    localStorage.setItem(data.login, JSON.stringify(data));
- }
+function saveAccount(data) {
+  localStorage.setItem(data.login, JSON.stringify(data));
+}
 
 function getAccount(key) {
-    const account = localStorage.getItem(key); 
-    
-    if(account) {
-        return JSON.parse(account);
-    }
-    return "";
- }
+  const account = localStorage.getItem(key);
 
- function saveSession(data, saveSession) {
-    if(saveSession) {
-        localStorage.setItem("session", data);
-    }
+  if (account) {
+    return JSON.parse(account);
+  }
+  return "";
+}
 
-    sessionStorage.setItem("logged", data);
+function saveSession(data, saveSession) {
+  if (saveSession) {
+    localStorage.setItem("session", data);
+  }
+
+  sessionStorage.setItem("logged", data);
 }
